c5afcf741476440f62ab5c32ca12045fb150427f
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2012-01-14 20:42:22 -0500

    Do not refresh home page ribbons on subsequent entries to the page (for now)

--- frontend/aiagallery/source/class/aiagallery/module/dgallery/home/Fsm.js
+++ frontend/aiagallery/source/class/aiagallery/module/dgallery/home/Fsm.js
@@ -65,12 +65,16 @@ qx.Class.define("aiagallery.module.dgallery.home.Fsm",
           "click" : "Transition_Idle_to_Idle_via_linkBoxClick",
 
           // Click on a featured app
-          "homeRibbonAppClick" : "Transition_Idle_to_Idle_via_homeRibbonAppClick",
+          "homeRibbonAppClick" : 
+            "Transition_Idle_to_Idle_via_homeRibbonAppClick",
           
-          // When we get an appear event, retrieve featured apps
+          // When we get an appear event, retrieve the featured apps. We
+          // only want to do it the first time, though, so we use a predicate
+          // to determine if it's necessary.
           "appear" :
           {
-            "main.canvas" : "Transition_Idle_to_AwaitRpcResult_via_appear"
+            "main.canvas" : 
+              qx.util.fsm.FiniteStateMachine.EventHandling.PREDICATE
           },
 
           // When we get a disappear event, stop our timer
@@ -84,6 +88,81 @@ qx.Class.define("aiagallery.module.dgallery.home.Fsm",
 
       // Replace the initial Idle state with this one
       fsm.replaceState(state, true);
+
+      // The following transitions have a predicate, so must be listed first
+
+      /*
+       * Transition: Idle to Idle
+       *
+       * Cause: "appear" on canvas
+       *
+       * Action:
+       *  Start our timer
+       */
+
+      trans = new qx.util.fsm.Transition(
+        "Transition_Idle_to_AwaitRpcResult_via_appear",
+      {
+        "nextState" : "State_AwaitRpcResult",
+
+        "context" : this,
+
+        "predicate" : function(fsm, event)
+        {
+          // Have we already been here before?
+          if (fsm.getUserData("noUpdate"))
+          {
+            // Yup. Don't accept this transition and no need to check further.
+            return null;
+          }
+          
+          // Prevent this transition from being taken next time.
+          fsm.setUserData("noUpdate", true);
+          
+          // Accept this transition
+          return true;
+        },
+
+        "ontransition" : function(fsm, event)
+        {          
+            
+          // Issue the remote procedure call to execute the query.
+          // In essence get the front page ribbons.
+          var request =
+              this.callRpc(fsm,
+                         "aiagallery.features",
+                         "getHomeRibbonData", [null]);
+
+          // When we get the result, we'll need to know what type of request
+          // we made.
+          request.setUserData("requestType", "getHomeRibbonData");
+        }
+      });
+
+      state.addTransition(trans);
+
+      /*
+       * Transition: Idle to Idle
+       *
+       * Cause: "disappear" on canvas
+       *
+       * Action:
+       *  Stop our timer
+       */
+
+      trans = new qx.util.fsm.Transition(
+        "Transition_Idle_to_Idle_via_disappear",
+      {
+        "nextState" : "State_Idle",
+
+        "context" : this,
+
+        "ontransition" : function(fsm, event)
+        {
+        }
+      });
+
+      state.addTransition(trans);
 
       /*
        * Transition: Idle to Idle
@@ -153,62 +232,6 @@ qx.Class.define("aiagallery.module.dgallery.home.Fsm",
       
       state.addTransition(trans);
       
-      /*
-       * Transition: Idle to Idle
-       *
-       * Cause: "appear" on canvas
-       *
-       * Action:
-       *  Start our timer
-       */
-
-      trans = new qx.util.fsm.Transition(
-        "Transition_Idle_to_AwaitRpcResult_via_appear",
-      {
-        "nextState" : "State_AwaitRpcResult",
-
-        "context" : this,
-
-        "ontransition" : function(fsm, event)
-        {          
-            
-          // Issue the remote procedure call to execute the query.
-          // In essence get the front page ribbons.
-          var request =
-              this.callRpc(fsm,
-                         "aiagallery.features",
-                         "getHomeRibbonData", [null]);
-
-          // When we get the result, we'll need to know what type of request
-          // we made.
-          request.setUserData("requestType", "getHomeRibbonData");
-        }
-      });
-
-      state.addTransition(trans);
-
-      /*
-       * Transition: Idle to Idle
-       *
-       * Cause: "disappear" on canvas
-       *
-       * Action:
-       *  Stop our timer
-       */
-
-      trans = new qx.util.fsm.Transition(
-        "Transition_Idle_to_Idle_via_disappear",
-      {
-        "nextState" : "State_Idle",
-
-        "context" : this,
-
-        "ontransition" : function(fsm, event)
-        {
-        }
-      });
-
-      state.addTransition(trans);
 
       
       // ------------------------------------------------------------ //
