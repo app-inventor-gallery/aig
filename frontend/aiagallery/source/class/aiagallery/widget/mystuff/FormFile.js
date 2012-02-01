65f3aa63e043cef71cf7e8e1f5d57a495cb53f7e
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2012-01-31 21:04:15 -0500

    Add new widget for (at present) selecting source file and displaying its name

---
+++ frontend/aiagallery/source/class/aiagallery/widget/mystuff/FormFile.js
@@ -0,0 +1,124 @@
+/**
+ * Copyright (c) 2012 Derrell Lipman
+ * 
+ * License:
+ *   LGPL: http://www.gnu.org/licenses/lgpl.html 
+ *   EPL : http://www.eclipse.org/org/documents/epl-v10.php
+ */
+
+qx.Class.define("aiagallery.widget.mystuff.FormFile",
+{
+  extend    : qx.ui.container.Composite,
+  implement :
+  [
+    qx.ui.form.IForm,
+    qx.ui.form.IStringForm
+  ],
+  include   : 
+  [
+    qx.ui.form.MForm
+  ],
+
+  construct : function(label, fieldName)
+  {
+    var             layout;
+
+    // Save the label and field name. The button child control will need them
+    this.label = label;
+    this.fieldName = fieldName;
+
+    layout = new qx.ui.layout.VBox(10);
+    this.base(arguments, layout);
+    
+    // Create the child controls
+    this.getChildControl("button");
+    this.getChildControl("filename");
+    
+    // Listen for changes to the 'required' property
+    this.addListener("changeRequired", this._onChangeRequired);
+  },
+
+  properties :
+  {
+    appearance :
+    {
+      refine   : true,
+      init     : "formimage"
+    },
+
+    value :
+    {
+      check    : "String",
+      apply    : "_applyValue",
+      nullable : false,
+      init     : null,
+      event    : "changeValue"
+    }
+  },
+
+  events :
+  {
+    /** Fired when the value was modified */
+    "changeValue" : "qx.event.type.Data",
+    
+    /** Fired when a filename selection is made */
+    "changeFileName" : "qx.event.type.Data"
+  },
+
+  members :
+  {
+    // property apply function
+    _applyValue : function(value, old)
+    {
+      this.getChildControl("filename").setValue(value);
+    },
+
+    /** Called when the 'required' property changes value */
+    _onChangeRequired : function(e)
+    {
+      var required;
+        
+      // Determine the string to append. (None, if not required)
+      required = (e.getData() ? " <span style='color:red'>*</span> " : "");
+
+      // Change the label to add or remove the red asterisk
+      this.getChildControl("button").setLabel(this.label + required);
+    },
+
+    // overridden
+    _createChildControlImpl : function(id, hash)
+    {
+      var             control;
+
+      switch(id)
+      {
+      case "button":
+        // Select file
+        control = new uploadwidget.UploadButton(this.fieldName, this.label);
+        control.setRich(true);
+        this._add(control);
+        
+        // When this button gets a changeFileName event, pass it along
+        control.addListener(
+          "changeFileName",
+          function(e)
+          {
+            this.fireDataEvent(e.getData());
+          },
+          this);
+        break;
+
+      case "filename":
+        control = new qx.ui.basic.Label();
+        control.set(
+          {
+            alignX : "center"
+          });
+        this._add(control, { flex : 1 });
+        break;
+      }
+
+      return control || this.base(arguments, id);
+    }
+  }
+});
