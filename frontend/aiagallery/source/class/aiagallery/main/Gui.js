ee6466ae3d65483162632af086593b3a38313c17
Derrell Lipman <derrell.lipman@unwireduniverse.com>
2012-02-16 10:48:33 -0500

    Channel open is working again

--- frontend/aiagallery/source/class/aiagallery/main/Gui.js
+++ frontend/aiagallery/source/class/aiagallery/main/Gui.js
@@ -418,12 +418,12 @@ qx.Class.define("aiagallery.main.Gui",
             // Load the Channel API. If we're on App Engine, it'll succeed
             var loader = new qx.bom.request.Script();
             loader.onload = 
-              function createChannel(status)
+              function createChannel()
               {
                 // Did we successfully load the Channel API?
-                switch(status)
+                switch(loader.status)
                 {
-                case "success":
+                case 200:
                   // Found the Channel API. Reqest a server push channel
                   rpc.callAsync(
                     function(e)
@@ -503,7 +503,7 @@ qx.Class.define("aiagallery.main.Gui",
                         channelMessage("close", data);
                         
                         // Re-establish the channel
-                        createChannel("success");
+                        createChannel();
                       };
 
                       // Save the channel token (if provided)
@@ -515,7 +515,7 @@ qx.Class.define("aiagallery.main.Gui",
 
                   default:
                     // Nope.
-                  _this.warn(status + ": Failed to load Channel API");
+                  _this.warn(loader.status + ": Failed to load Channel API");
                   break;
 
                 }
