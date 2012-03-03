2f1caf9e62a23a2a3a47b0cc343742577818caec
Michael Dawson <mijoda@gmail.com>
2012-03-03 16:24:34 -0500

    Re-order app table columns, and sort by descending number of flags.

--- frontend/aiagallery/source/class/aiagallery/module/mgmt/applications/Gui.js
+++ frontend/aiagallery/source/class/aiagallery/module/mgmt/applications/Gui.js
@@ -102,9 +102,9 @@ qx.Class.define("aiagallery.module.mgmt.applications.Gui",
             colSet  : { width : "2*" }
           },
           {
-            heading : this.tr("Tags"),
-            id      : "tags",
-            colSet  : { width : 120 }
+            heading : this.tr("Flags"),
+            id      : "numCurFlags",
+            colSet  : { width : 50 }
           },
           {
             heading : this.tr("Status"),
@@ -112,9 +112,9 @@ qx.Class.define("aiagallery.module.mgmt.applications.Gui",
             colSet  : { width : 50 }
           },
           {
-            heading : this.tr("Flags"),
-            id      : "numCurFlags",
-            colSet  : { width : 50 }
+            heading : this.tr("Tags"),
+            id      : "tags",
+            colSet  : { width : 120 }
           },
           {
             heading : this.tr("Views"),
@@ -319,6 +319,7 @@ qx.Class.define("aiagallery.module.mgmt.applications.Gui",
       var             requestType = rpcRequest.getUserData("requestType");
       var             cellEditor;
       var             table;
+      var             model;
       var             deletedRow;
 
       if (response.type == "failed")
@@ -334,14 +335,54 @@ qx.Class.define("aiagallery.module.mgmt.applications.Gui",
       {
       case "getAppListAll":
         table = fsm.getObject("table");
+        model = table.getTableModel();
 
         // Set the entire data model given the result array
         // 2nd parameter, "rememberMaps", set to true (default: false), so that
         // columns not in the model are still accessible
-        // (such as uid, e.g. when deleting an app).
+        // (such as uid, importantly).
         // The 3rd param "clearSorting", is changed from T default to F so
         // that sorting is preserved)
-        table.getTableModel().setDataAsMapArray(response.data.result.apps, true, false);
+        model.setDataAsMapArray(response.data.result.apps, true, false);
+
+        // Sort the table by flags, descending
+
+        // Sort functions that treat non-numbers, specifically 'undefined', as zero
+        // Verified that this is required to correctly order the sim data, where
+        // numCurFlags is usually undefined.
+        // Probably unnecessary in build environment where everything's 
+        // initialized to 0
+        var ascending = function(row1,row2)
+        {
+          var col = arguments.callee.columnIndex;
+          var arg1 = row1[col];
+          var i = qx.lang.Type.isNumber(arg1) ? arg1 : 0;
+          var arg2 = row2[col];
+          var j = qx.lang.Type.isNumber(arg2) ? arg2 : 0;
+          return (i > j) ? 1 : ((i == j) ? 0 : -1);
+        };
+        var descending = function(row1,row2)
+        {
+          var col = arguments.callee.columnIndex;
+          var arg1 = row1[col];
+          var i = qx.lang.Type.isNumber(arg1) ? arg1 : 0;
+          var arg2 = row2[col];
+          var j = qx.lang.Type.isNumber(arg2) ? arg2 : 0;
+          return (i > j) ? -1 : ((i == j) ? 0 : 1);
+        };
+
+        // Column to sort on.
+        var flagColumn = model.getColumnIndexById("numCurFlags");
+        // How to sort.  Specifying this as a single function rather than a map of
+        // 2 functions did not work, contradicting API docs for
+        // qx.ui.table.model.Simple#setSortMethods()
+        model.setSortMethods(flagColumn, {
+            ascending  : ascending,
+            descending : descending
+          });
+        // Sort, in descending order.
+        model.sortByColumn(flagColumn, false);
+
 
         // Save the category list in a known place, for later access
         // FIXME (maybe): Same place in the myapps module does this same thing.
