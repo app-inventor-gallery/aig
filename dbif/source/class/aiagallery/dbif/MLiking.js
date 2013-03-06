f8de301a4371e4c25a50ab9fff3ff3d6ea0e3180
Paul Geromini <cpuwhiz11@gmail.com>
2013-03-06 16:10:52 -0500

    Fix likes not being added to the cache making it appear a user did not like an app.

--- dbif/source/class/aiagallery/dbif/MLiking.js
+++ dbif/source/class/aiagallery/dbif/MLiking.js
@@ -6,6 +6,9 @@
  *   EPL : http://www.eclipse.org/org/documents/epl-v10.php
  */
 
+/*
+#ignore(com.google.*)
+ */
 qx.Mixin.define("aiagallery.dbif.MLiking",
 {
   construct : function()
@@ -137,6 +140,35 @@ qx.Mixin.define("aiagallery.dbif.MLiking",
                                visitorDataObj.email, appDataObj);
 
               }
+            }
+
+            // Update the likes in the cache with this new like by the user
+            switch (liberated.dbif.Entity.getCurrentDatabaseProvider())
+            {
+            case "appengine":
+              var memcacheServiceFactory =
+              Packages.com.google.appengine.api.memcache.MemcacheServiceFactory;
+
+              var syncCache = memcacheServiceFactory.getMemcacheService();
+              var serialLike = JSON.stringify([likesDataObj]);
+
+              // Expiration date
+              var calendarClass = java.util.Calendar;
+              var date = calendarClass.getInstance();
+              date.add(calendarClass.DATE, 1);
+
+              var expirationClass 
+                = com.google.appengine.api.memcache.Expiration;  
+              var expirationDate = expirationClass.onDate(date.getTime());
+
+              syncCache.put("retlikes_".concat(appId),
+                serialLike, expirationDate);
+
+              break;
+
+            default:
+              // Not on appengine
+              break;
             }
 
             // Write it back to the database
