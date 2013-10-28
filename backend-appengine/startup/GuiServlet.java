0190684c88e1e3bebe87ff19daeb68ab9f17e5da
Karuto Z <iamkaruto@gmail.com>
2013-10-27 22:40:19 -0400

    Updated analytics code to fit MIT version, changed datastore indexes

--- backend-appengine/startup/GuiServlet.java
+++ backend-appengine/startup/GuiServlet.java
@@ -62,7 +62,7 @@ public class GuiServlet extends HttpServlet
 			"	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
 			"	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
 			"	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
-			"	  ga('create', 'UA-41636874-1', 'usf-appinventor-gallery.appspot.com');" +
+			"	  ga('create', 'UA-41636874-2', 'mit.edu');" +
 			"	  ga('send', 'pageview');" +
 			"	 </script>" +
             "  </head>" +
